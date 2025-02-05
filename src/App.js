380076681b0949cbcb205e019e2c45f333c29a84
Sapir Yanai <sapir9233@gmail.com>
2025-02-05 18:39:44 +0200

    some message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import SignLanguageRecognition from './components/SignLanguageRecognition';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import FeedScreen from './feed/FeedScreen';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal } from 'bootstrap';
+import './Login.css';
+import { authenticateUser } from './Users';
+import { LoggedIn } from './loggedIn';
+import MyProfilePage from './feed/MyProfilePage';
+import FriendPage from './feed/FriendPage';
 
 function App() {
+  const [displayName, setDisplayName] = useState('');
+  const [profilePicture, setProfilePicture] = useState('');
+  const [username, setUsername] = useState('');
+  const { loggedIn, setLoggedIn } = LoggedIn();
+
+  // Helper function to handle logged-in redirects
+  const ProtectedRoute = ({ children }) => {
+    return loggedIn ? children : <Navigate replace to="/" />;
+  };
+
   return (
-    <div className="App">
-      <h1>ברוכים הבאים לאפליקציה לזיהוי שפת הסימנים!</h1>
-      <SignLanguageRecognition />
-    </div>
+    <Router>
+      <Routes>
+        {/* Route for login page */}
+        <Route path="/" element={<Login setLoggedIn={setLoggedIn} setUsername={setUsername} setDisplayName={setDisplayName} setProfilePicture={setProfilePicture} />} />
+
+        {/* Public About route */}
+       
+
+        {/* Protected Routes */}
+        <Route path="/MyProfilePage" element={<ProtectedRoute><MyProfilePage displayName={displayName} username={username} profilePicture={profilePicture} /></ProtectedRoute>} />
+        <Route path="/FriendPage" element={<ProtectedRoute><FriendPage /></ProtectedRoute>} />
+        <Route path="/feed" element={<ProtectedRoute><FeedScreen setLoggedIn={setLoggedIn} username={username} displayName={displayName} profilePicture={profilePicture} /></ProtectedRoute>} />
+
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
