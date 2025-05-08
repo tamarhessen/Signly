11d0716bc06ba7aa117e19723f4059afae772044
tamarhessen <tamarhessen@gmail.com>
2025-05-08 18:01:07 +0300

    added

--- src/home/RightPanel.js
+++ src/home/RightPanel.js
@@ -5,6 +5,7 @@ function RightPanel({ username, level, points, nextLevelPoints, navigate }) {
   const [lives, setLives] = useState(3);
   const [timeLeft, setTimeLeft] = useState(null);
 
+
   let pointsperlevel = (points - ((level - 1) * 26));
 
   async function fetchLives(abortSignal = null) {
@@ -120,7 +121,12 @@ function RightPanel({ username, level, points, nextLevelPoints, navigate }) {
         </p>
 
         {lives === 0 && timeLeft !== null && timeLeft > 0 && (
+
           <p>⏳ Please wait: {formatTime(timeLeft)}</p>
+
+       
+         // <p className="time-wait">⏳ Please wait: {formatTime(timeLeft)}</p>
+
         )}
       </div>
     </div>
--- start_servers.ps1
+++
@@ -1,8 +0,0 @@
-# Start Flask (Python) server
-#Start-Process -NoNewWindow -FilePath "cmd" -ArgumentList "/c cd python && python camera.py"
-
-# Start Node.js backend (server/)
-Start-Process -NoNewWindow -FilePath "cmd" -ArgumentList "/c cd server && npm run server"
-
-# Start React frontend (main directory)
-Start-Process -NoNewWindow -FilePath "cmd" -ArgumentList "/c npm start"
